45a89cd314d5aeb65ebfaf4b874cfd345a759878
JCB <jeanclaude.bartier@gmail.com>
2015-10-10 18:44:24 +0200

    maj

--- Cartographie/Routines/lib_carto.R
+++ Cartographie/Routines/lib_carto.R
@@ -4,6 +4,7 @@
 # ---------- package pour lire/écrire/manipuler des données spatiales -----------------------
 library("maptools") # contient les fonctions d'overlay
 library("rgrs")
+library("questionr")
 library("rgeos") #nécessaire pour ggplot2::fortify.sp
 library("rgdal") # contient read/writeOGR pour lire les shapefiles et read/writeRGDAL pour lire les raster
 library("spdep") # fonctions statistiques spatiales utiles
